69069bb413ec0e470f330c7c9996a680af5d9c8e
PHILOSOPHICA <83201576+PHILOSOPHICA@users.noreply.github.com>
2021-09-14 12:00:09 -0500

    Add files via upload

---
+++ JS/anim.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.Datos', {
+  duration: 5000,
+  distance: '-100px',
+  reset: true,
+})
+
+sr.reveal('.subt', {
+  duration: 4000,
+  distance: '20px',
+  reset: true,
+})
+
+sr.reveal('.foto', {
+  duration: 4000,
+  origin: 'right',
+  distance: '-100px',
+  reset: true,
+})
+
+sr.reveal('.ponente', {
+  duration: 4000,
+  origin: 'left',
+  distance: '-100px',
+  reset: true,
+})
+
+sr.reveal('.sint', {
+  duration: 4000,
+  origin: 'right',
+  distance: '-100px',
+  reset: true,
+})
+
+sr.reveal('.vínculos', {
+  duration: 4000,
+  origin: 'left',
+  distance: '-100px',
+  reset: true,
+})
